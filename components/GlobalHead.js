15cb2501008e75d7df81e9600f996f299193c767
karryzk <karry.l.zhang@gmail.com>
2024-11-13 15:00:04 +0900

    Update GlobalHead.js

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -95,6 +95,162 @@ const GlobalHead = props => {
 
   return (
     <Head>
+      {/* 现有的meta标签和其他标签保持不变 */}
+      
+      {/* 添加Mouseflow监控脚本 */}
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          window._mfq = window._mfq || [];
+          (function() {
+            var mf = document.createElement("script");
+            mf.type = "text/javascript";
+            mf.defer = true;
+            mf.src = "//cdn.mouseflow.com/projects/6faca729-8b29-4171-8a70-ad6fe0224a99.js";
+            document.getElementsByTagName("head")[0].appendChild(mf);
+          })();
+        `
+      }} />
+      
+      {/* 其他现有的标签保持不变 */}
+      {COMMENT_WEBMENTION_ENABLE && (
+        // ... 现有的webmention相关代码 ...
+      )}
+      
+      {/* 其余代码保持不变 */}
+    </Head>
+  )
+}
+
+// ... 其余代码保持不变 ...
+主要改动说明：
+
+我们使用 dangerouslySetInnerHTML 来插入脚本内容，这是React中插入原始HTML的推荐方式。
+
+脚本被放置在 <Head> 组件内，确保它会被正确地添加到页面的 <head> 部分。
+
+保持了原有的其他所有功能不变。
+
+这样修改后，Mouseflow的监控脚本将会在页面加载时正确执行。请注意，由于这是在客户端执行的脚本，它会在页面加载后开始工作。
+
+另外，确保你的项目的内容安全策略（CSP）允许加载来自 cdn.mouseflow.com 的脚本。如果你的网站有严格的CSP设置，可能需要相应地调整它们。
+
+
+
+
+Karry Zhang
+Untitled-1.js
+
+
+@Untitled-1.js 我实际部署打包时报错，能否提供下完整的代码
+
+
+
+
+Monica
+Claude 3.5 Sonnet
+1 个上下文条目
+我注意到有一些语法错误和不完整的标签。这里是修正后的完整代码：
+
+Untitled-1.js
+../karryz
+
+应用
+
+
+import { siteConfig } from '@/lib/config'
+import { useGlobal } from '@/lib/global'
+import { loadExternalResource } from '@/lib/utils'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+/**
+ * 页面的Head头，有用于SEO
+ * @param {*} param0
+ * @returns
+ */
+const GlobalHead = props => {
+  const { children, siteInfo, post, NOTION_CONFIG } = props
+  let url = siteConfig('PATH')?.length
+    ? `${siteConfig('LINK')}/${siteConfig('SUB_PATH', '')}`
+    : siteConfig('LINK')
+  let image
+  const router = useRouter()
+  const meta = getSEOMeta(props, router, useGlobal()?.locale)
+  if (meta) {
+    url = `${url}/${meta.slug}`
+    image = meta.image || '/bg_image.jpg'
+  }
+  const title = meta?.title || siteConfig('TITLE')
+  const description = meta?.description || `${siteInfo?.description}`
+  const type = meta?.type || 'website'
+  const lang = siteConfig('LANG').replace('-', '_')
+  const category = meta?.category || siteConfig('KEYWORDS')
+  const favicon = siteConfig('BLOG_FAVICON')
+  const webFontUrl = siteConfig('FONT_URL')
+  const BACKGROUND_DARK = siteConfig('BACKGROUND_DARK', '', NOTION_CONFIG)
+
+  const SEO_BAIDU_SITE_VERIFICATION = siteConfig(
+    'SEO_BAIDU_SITE_VERIFICATION',
+    null,
+    NOTION_CONFIG
+  )
+
+  const SEO_GOOGLE_SITE_VERIFICATION = siteConfig(
+    'SEO_GOOGLE_SITE_VERIFICATION',
+    null,
+    NOTION_CONFIG
+  )
+
+  const BLOG_FAVICON = siteConfig('BLOG_FAVICON', null, NOTION_CONFIG)
+
+  const COMMENT_WEBMENTION_ENABLE = siteConfig(
+    'COMMENT_WEBMENTION_ENABLE',
+    null,
+    NOTION_CONFIG
+  )
+
+  const COMMENT_WEBMENTION_HOSTNAME = siteConfig(
+    'COMMENT_WEBMENTION_HOSTNAME',
+    null,
+    NOTION_CONFIG
+  )
+  const COMMENT_WEBMENTION_AUTH = siteConfig(
+    'COMMENT_WEBMENTION_AUTH',
+    null,
+    NOTION_CONFIG
+  )
+  const ANALYTICS_BUSUANZI_ENABLE = siteConfig(
+    'ANALYTICS_BUSUANZI_ENABLE',
+    null,
+    NOTION_CONFIG
+  )
+
+  const FACEBOOK_PAGE = siteConfig('FACEBOOK_PAGE', null, NOTION_CONFIG)
+  let keywords = meta?.tags || siteConfig('KEYWORDS')
+  if (post?.tags && post?.tags?.length > 0) {
+    keywords = post?.tags?.join(',')
+  }
+
+  useEffect(() => {
+    loadExternalResource(
+      'https://cdnjs.cloudflare.com/ajax/libs/webfont/1.6.28/webfontloader.js',
+      'js'
+    ).then(url => {
+      const WebFont = window?.WebFont
+      if (WebFont) {
+        console.log('LoadWebFont', webFontUrl)
+        WebFont.load({
+          custom: {
+            urls: webFontUrl
+          }
+        })
+      }
+    })
+  }, [])
+
+  return (
+    <Head>
       <link rel='icon' href={favicon} />
       <title>{title}</title>
       <meta name='theme-color' content={BACKGROUND_DARK} />
@@ -129,8 +285,11 @@ const GlobalHead = props => {
       <meta name='twitter:description' content={description} />
       <meta name='twitter:title' content={title} />
       <link rel='icon' href={BLOG_FAVICON} />
-      <script src="https://js.ptengine.jp/3dboyxf1.js"><script>
-      {/* 添加Mouseflow监控脚本 */}
+      
+      {/* PTEngine 监控脚本 */}
+      <script src="https://js.ptengine.jp/3dboyxf1.js"></script>
+      
+      {/* Mouseflow 监控脚本 */}
       <script dangerouslySetInnerHTML={{
         __html: `
           window._mfq = window._mfq || [];
@@ -142,7 +301,7 @@ const GlobalHead = props => {
             document.getElementsByTagName("head")[0].appendChild(mf);
           })();
         `
-     }} />
+      }} />
        
       {COMMENT_WEBMENTION_ENABLE && (
         <>
